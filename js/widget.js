344435dc73729f75a5ea8468fb2a4338f47cd4e8
marcuspgraham <64594348+marcuspgraham@users.noreply.github.com>
2020-11-13 08:34:07 +0000

    13/11/20_1

--- js/widget.js
+++ js/widget.js
@@ -13,9 +13,48 @@ to complete</p>
 alertBanner.addEventListener('click', e => {
     const element = e.target;
     if (element.classList.contains("alert-banner-close")) {
-    alert.style.display = "none"
+        alert.style.display = "none"
     }
 });
 
-// Chart Widgets
+// Line Graph
+
+let trafficCanvas = document.getElementById("traffic-chart");
+
+let trafficData = {
+        labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3","4-10", "11-17", "18-24", "25-31"],
+        datasets: [{
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 2000, 2500],
+        backgroundColor: 'rgba(116, 119, 191, .3)',
+        borderWidth: 1,
+    }]
+};
+
+let trafficOptions = {
+    aspectRatio: 2.5,
+        animation: {
+            duration: 0
+    },
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+        }
+      }]
+    },
+    legend : {
+        display: false
+    }
+};
+
+let trafficChart = new Chart(trafficCanvas, {
+        type: 'line',
+        data: trafficData,
+        options: trafficOptions
+});
+
+
+// Bar Graph
+
+const dailyCanvas = document.getElementById("daily-chart");
 
