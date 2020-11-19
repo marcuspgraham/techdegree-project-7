d97bed9ea77f76bb26b80bd88fdd75b8b9c5db97
marcuspgraham <64594348+marcuspgraham@users.noreply.github.com>
2020-11-19 19:44:36 +0000

    19_11_20_3

--- js/widget.js
+++ js/widget.js
@@ -13,7 +13,7 @@ to complete</p>
 alertBanner.addEventListener('click', e => {
     const element = e.target;
     if (element.classList.contains("alert-banner-close")) {
-        alert.style.display = "none"
+        alertBanner.style.display = "none"
     }
 });
 
